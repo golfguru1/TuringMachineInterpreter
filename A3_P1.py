1296c15083c0349b721c17003a4d6322c28fe757
Mark Hall <mhall48@uwo.ca>
2015-03-15 13:43:00 -0400

    now accepts input if a final state is reached, added comments

--- A3_P1.py
+++ A3_P1.py
@@ -3,6 +3,7 @@ instructions = []
 finalStates = []
 inputs = []
 
+#populate the list of instructions, final states and inputs
 for line in sys.stdin:
     a = line.strip("\r\n").split(" ")
     if(a[0]=='t'):
@@ -14,22 +15,27 @@ for line in sys.stdin:
     	myList.insert(0,"Z")
     	myList.append("Z")
     	inputs.append(myList)
-
+print inputs
+#get the list of all current state and input symbol combinations
 instructionInputArray=[]
 for z in xrange(0,len(instructions)):
 	instructionInputArray.append(instructions[z][0]+instructions[z][1])
 
+#use the above array to make a dictionary that maps each currentState+inputSymbol to the corresponding insruction
 instructionDict = {}
 for i in xrange(0,len(instructionInputArray)):
 	instructionDict[instructionInputArray[i]] = instructions[i]
 
 found=False;
+#loop through every input line
 for inputArray in inputs:
+	#reset the current state and head for each set of inputs
 	curState = 0
 	head = 1
 	found = False
+	#loop until a Halt instruction is found, until there is no transition or until we reach a final state
 	while not found:
-		#get the instruction based on the current state and value of inputArray[head]
+		#get the instruction based on the current state and value of inputArray at the current head
 		if(str(curState)+inputArray[head] in instructionInputArray):
 			instructionSet = instructionDict.get(str(curState)+inputArray[head])
 			curState=instructionSet[2]
@@ -42,8 +48,13 @@ for inputArray in inputs:
 				found=True;
 		else:
 			found=True;
+		if str(curState) in finalStates[0]:
+			found=True
+	#print the resulting input string
 	print "".join(inputArray[1:len(inputArray)-1])
+	#if the ending state is in the list of accepting state, print ACCEPTED
 	if str(curState) in finalStates[0]:
 		print "ACCEPTED"
+	#otherwise print REJECTED
 	else:
 		print "REJECTED"
